7e1e0c11e079851b1a91f7b544b2d5aceb6d7c30
Adam Griffin <adam.griffin@windermeresolutions.com>
2013-02-24 21:29:13 -0800

    Main features now working

--- lib/nombus.rb
+++ lib/nombus.rb
@@ -2,7 +2,17 @@ require "nombus/version"
 
 module Nombus
   DefaultSeparator = ','
+  # Using Google's public namerservers by default for DNS lookups.
+  # There are several domains that were configured on nsmaster
+  # that actually are not pointing at our nameserver anymore.
+  # Using the defaults was masking this problem when running
+  # the script on our network.
   DefaultNameservers = '8.8.8.8 8.8.4.4'
   OutputFileName = 'nombus_domains.csv'
   Column = '0'
+  def Nombus.NotManagedByUs?(our_ns, their_ns, our_ips, their_ip)
+    # Return true if it's not our nameserver,
+    # but does use one of the old a.com IPs.
+  	(our_ns != their_ns) and (our_ips.include? their_ip)
+  end
 end
--- lib/wre_dns.rb
+++ lib/wre_dns.rb
@@ -1,16 +1,11 @@
 require 'dnsruby'
 
-class WreDns
-  # Class contstants & methods specific to Windermere's DNS
+module WreDns
+  # Constants specific to Windermere's DNS
   NameServer = Dnsruby::Name.create('nsmaster.windermere.com.')
   Slave1 = Dnsruby::Name.create('ns1.windermere.com.')
   Slave2 = Dnsruby::Name.create('ns2.windermere.com.')
   Slave3 = Dnsruby::Name.create('ns3.windermere.com.')
   OldAcomIps = [ Dnsruby::IPv4.create('50.18.188.104'), Dnsruby::IPv4.create('184.72.38.12') ]
   AcomIp = Dnsruby::IPv4.create('205.234.73.173')
-  def WreDns.NotManagedByUs?(their_ns, their_ip)
-    # Return true if it's not our nameserver,
-    # but does use one of the old a.com IPs.
-  	(NameServer != their_ns) and (OldAcomIps.include? their_ip)
-  end
 end
