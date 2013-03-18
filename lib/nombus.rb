62a6a914dc7ac16133178523edf2ec31ac46ec16
Adam Griffin <adam.griffin@windermeresolutions.com>
2013-03-17 20:39:12 -0700

    Refactored, adding Configurator & LookerUpper classes

--- lib/nombus.rb
+++ lib/nombus.rb
@@ -1,91 +1,94 @@
 require "nombus/version"
-require 'whois'
+require "dnsruby"
+require "whois"
+require "wre_dns"
 
 module Nombus
   
-  include Methadone::CLILogging
-  include Methadone::ExitNow
-  
-  DefaultSeparator = ','
-  # Using Google's public namerservers by default for DNS lookups.
-  # There are several domains that were configured on nsmaster
-  # that actually are not pointing at our nameserver anymore.
-  # Using the defaults was masking this problem when running
-  # the script on our network.
-  DefaultNameservers = '8.8.8.8 8.8.4.4'
-  Column = '1'
-  FailHeaders = ["Domain", "Error"]
-  SuccessColor = :green
-  DebugColor = :magenta
-  WarnColor = :yellow
-  ErrorColor = :red
-  
-  def Nombus.GetRecords(records)
-    # Get the A record, and Nameserver from an array of resource records
-    a_record = nil
-    nameserver = nil		
-    records.each do |rr|
-      if rr.is_a? Dnsruby::RR::IN::A
-    		a_record = rr.address
-    	elsif rr.is_a? Dnsruby::RR::SOA
-    		nameserver = rr.mname
-    	end
+  class Configurator
+    
+    DefaultSeparator = ','
+    # Using Google's public namerservers by default for DNS lookups.
+    # There are several domains that were configured on nsmaster
+    # that actually are not pointing at our nameserver anymore.
+    # Using the defaults was masking this problem when running
+    # the script on our network.
+    DefaultNameservers = '8.8.8.8 8.8.4.4'
+    Column = '1'
+    FailHeaders = ["Domain", "Error"]
+    SuccessColor = :green
+    DebugColor = :magenta
+    WarnColor = :yellow
+    ErrorColor = :red
+    
+    def check_column(column)
+      case column
+      when /\D/
+        "Error: #{column} is not a valid number"
+      when '0'
+        "Error: column number must be greater than 0"
+      end
     end
-    return nameserver, a_record
-  end
-  
-  def Nombus.NotManagedByUs?(our_ns, their_ns, our_ips, their_ip)
-    # Return true if it's not our nameserver,
-    # but does use one of the old a.com IPs.
-  	(our_ns != their_ns) and (our_ips.include? their_ip)
-  end
-  
-  def Nombus.NotPointedAtUs?(our_ips, their_ip)
-    not our_ips.include? their_ip
-  end
-  
-  def Nombus.LookupErrorMessage(domain, error)
-    # Lookup with whois first to see if domain is even registered
-    record = Whois.whois(domain.to_s)
-    if record.available?
-      return "#{domain}: Not a registered domain name"
+    
+    def get_column(column)
+      # Internally csv starts at 0, but command option starts at 1, & is string
+      column.to_i - 1
     end
-    # Try to explain other errors as best as possible
-    case error
-    when Dnsruby::NXDomain
-      return "No records found for #{domain}"
-    when Dnsruby::ServFail
-      return "Lookup failed for #{domain}"
-    when Dnsruby::ResolvError
-      return "DNS result has no information for #{domain}"
+    
+    def check_separator(separator)
+      case separator
+      when /\s/
+        "Error: Separator can't be a literal whitspace character. Use 'tab' for tabs"
+      end
     end
+    
+    def get_separator(separator)
+      case separator
+      when 'tab' # Can't use literal tab on command line
+        return "\t"
+      else
+        separator
+      end
+    end
+    
   end
+  
+  class LookerUpper < Dnsruby::DNS
+    include Dnsruby
+    include WreDns
 
-  def Nombus.GetColumnIndex(column)
-    # Handles error checking of column number passed on comamnd line
-    # Returns index as proper type and value for indexing csv array
-    if column =~ /[0-9]/
-      index = column.to_i
-    else
-      exit_now! "Error: #{column} is not a number"
+    def get_records(rr)
+      nameserver = rr.find {|r| r.type == 'SOA'}.mname
+      a_record = rr.find {|r| r.type == 'A'}.address
+      return nameserver, a_record
     end
-    if index < 1
-      exit_now! "Error: column number must be greater than 0"
-    end
-    # Internally csv starts at 0, but want command option to start at 1, so convert it
-    index - 1
-  end
 
-  def Nombus.GetSeparator(separator)
-    case separator
-    when /[\s\t\r\n\f]/
-      exit_now! "Error: Separator can't be a whitspace character. Use 'tab' for tabs"
-    # Can't use \t on command line
-    when 'tab'
-      return "\t"
-    else
-      return separator
+    def not_managed_by_us?(ns, ip)
+      # Return true if it's not our nameserver,
+      # but does use one of the old a.com IPs.
+    	(NameServer::Master != ns) && (AgentWebsites::Old_acom_ips.include? ip)
     end
+
+    def not_pointed_at_us?(their_ip)
+      not AgentWebsites::All_acom_ips.include? their_ip
+    end
+
+    def lookup_error_message(domain, error)
+      # See if domain is even registered 1st
+      if Whois.whois(domain.to_s).available?
+        return "#{domain}: Not a registered domain name"
+      end
+      # Try to explain other errors as best as possible
+      case error
+      when NXDomain
+        return "No records found for #{domain}"
+      when ServFail
+        return "Lookup failed for #{domain}"
+      when ResolvError
+        return "DNS result has no information for #{domain}"
+      end
+    end
+
   end
 
 end
--- lib/wre_dns.rb
+++ lib/wre_dns.rb
@@ -1,13 +1,19 @@
-require 'dnsruby'
 
-module WreDns
-  # Constants specific to Windermere's DNS
-  NameServer = Dnsruby::Name.create('nsmaster.windermere.com.')
-  Slave1 = Dnsruby::Name.create('ns1.windermere.com.')
-  Slave2 = Dnsruby::Name.create('ns2.windermere.com.')
-  Slave3 = Dnsruby::Name.create('ns3.windermere.com.')
-  OldAcomIps = [ Dnsruby::IPv4.create('50.18.188.104'), Dnsruby::IPv4.create('184.72.38.12') ]
-  AcomIp = Dnsruby::IPv4.create('205.234.73.173')
-  PawsIp = Dnsruby::IPv4.create('205.234.73.177')
-  AllAcomIps = OldAcomIps << AcomIp << PawsIp
+module WreDns 
+  # Classes & constants specific to Windermere's DNS
+  
+  class NameServer
+      Master = 'nsmaster.windermere.com.'
+      Slave1 = 'ns1.windermere.com.'
+      Slave2 = 'ns2.windermere.com.'
+      Slave3 = 'ns3.windermere.com.'
+  end
+  
+  class AgentWebsites
+    Acom_ip = '205.234.73.173.'
+    Old_acom_ips = ['50.18.188.104.', '184.72.38.12.']
+    Paws_IP = '205.234.73.177'
+    All_acom_ips = Old_acom_ips << Acom_ip << Paws_IP
+  end
+
 end
